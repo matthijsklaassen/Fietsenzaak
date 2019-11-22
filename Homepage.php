ae6f04ff237f884628ddd344f015fdcef46a2add
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-11-22 09:58:58 +0100

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -22,12 +22,13 @@ echo <html xmlns="http://www.w3.org/1999/xhtml">
       </ul>
     </div>
 
-    <img class="frontpicture" src="Link naar afbeelding" alt="Typ hier uw tekst">
+    <img class="frontpicture" src="www.anwb.nl/binaries/content/gallery/anwb/portal/fietsen/fietsroutes/fietsen-vader-kinderen-zomer-lente.jpg/fietsen-vader-kinderen-zomer-lente.jpg/anwb%3Aw1520" 
+                              alt="Een vader en moeder die met hun zoon door de prachtige Nederlandse polders fietsen tijdens een mooie zomerdag">
       <!-- alt: wanneer het plaatje niet weergegeven kan worden, wordt dit als alternatieve tekst weergegeven
            src: welk plaatje / waar komt het plaatje vandaan-->
     
     <p id="slogan">
-      Fietsenzaak PHP </br></p>
+      De lekke band </br></p>
     
   </body>
   
