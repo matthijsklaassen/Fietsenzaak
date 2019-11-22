258c34f701e72b81b0c3d642cfb480f880bc4294
lduynkerke <57993503+lduynkerke@users.noreply.github.com>
2019-11-22 09:45:27 +0100

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -1,3 +1,37 @@
 <?php
+echo <html xmlns="http://www.w3.org/1999/xhtml">
 
+  <head>
+    
+    <link href="CSSFILE.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8">
+    
+    <title>Typ hier uw tekst</title>
+    
+  </head>
+
+  <body id="home">
+  
+    <!-- Menubalk -->
+    <div id="menubalk">
+      <ul>
+        <li><a class="active" href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+        <li><a href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+        <li><a href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+        <li><a href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+        <li><a href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+        <li><a href="Typ hier uw tekst.html">Typ hier uw tekst</a></li>
+      </ul>
+    </div>
+
+    <img class="frontpicture" src="Link naar afbeelding" alt="Typ hier uw tekst">
+      <!-- alt: wanneer het plaatje niet weergegeven kan worden, wordt dit als alternatieve tekst weergegeven
+           src: welk plaatje / waar komt het plaatje vandaan-->
+    
+    <p id="slogan">
+      Typ hier uw tekst </br></p>
+    
+  </body>
+  
+</html>";
 ?>
