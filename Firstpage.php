b368b8dfd015af16d0664d26bc13bbf14955ab81
lduynkerke <57993503+lduynkerke@users.noreply.github.com>
2019-11-25 15:51:07 +0100

    Update Firstpage.php

--- Firstpage.php
+++ Firstpage.php
@@ -1,1 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        
+        <link href="CSSFILE.css" rel="stylesheet" type="text/css"/>
+        <meta charset="utf-8">
+        
+        <title>Spelregels</title>
+        
+    </head>
+    
+    <body>
+
+        <!-- Menubalk -->
+        <div id="menubalk">
+          <ul>
+            <li><a href="Homepage.php">Home</a></li>
+            <li><a class="active" href="fomrulier.php">Spelregels</a></li>
+            <li><a href="Secondpage.php">Toevoegquery PK</a></li>
+            <li><a href="Thirdpage.php">Extra PHP dingen</a></li>
+          </ul>
+        </div>
+            
+        <h1> Formulier </h1>
+        <hr>
+        <p>
+            Op deze pagina is het mogelijk om de database aan te passen. 
+            Door middel van het onderstaande formulier kunnen er nieuwe fietsen, nieuwe klanten, nieuwe orders worden ingevuld.
+            Zo is het mogelijk om de database op een efficiënte manier up to date te houden.
+        </p>             
+    
+    </body> 
+
+</html>
+
+<?php 
+// Backend van het formulier
+
+
+?>
