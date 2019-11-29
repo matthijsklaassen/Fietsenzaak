2ded83979f63c86aa08be5c9142f7b349c229701
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-11-29 09:58:20 +0100

    Update and rename Secondpage.php to ConnectionDatabase.php

---
+++ ConnectionDatabase.php
@@ -0,0 +1,4 @@
+<?php
+$db = new PDO ('mysql:host=https://auth-db307.hostinger.com/db_routines.php?db=u392629804_groep1;dbname=u392629804_groep1', 'u392629804_usergroep1', 'RwW$o1lQ');
+
+?>
--- Secondpage.php
+++
@@ -1,4 +0,0 @@
-<?php
-$db = new PDO ("mysql:host=127.0.0.1;dbname=u392629804_groep1", "u392629804_usergroep1", "RwW$o1lQ");
-
-?>
