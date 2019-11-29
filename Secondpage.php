6bbe9a23bf2fcde8646678f50c876f0ae21771d7
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-11-29 09:52:18 +0100

    Update Secondpage.php

--- Secondpage.php
+++ Secondpage.php
@@ -1,4 +1,4 @@
 <?php
-$db = new PDO ("mysql:host=localhost;dbname=u392629804_groep1", "u392629804_usergroep1", "RwW$o1lQ");
+$db = new PDO ("mysql:host=127.0.0.1;dbname=u392629804_groep1", "u392629804_usergroep1", "RwW$o1lQ");
 
 ?>
