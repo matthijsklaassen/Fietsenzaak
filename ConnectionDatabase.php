531f9d245e22383e30e628ab44ff95b07407d706
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-11-29 10:10:27 +0100

    Update ConnectionDatabase.php

--- ConnectionDatabase.php
+++ ConnectionDatabase.php
@@ -2,3 +2,19 @@
 $db = new PDO ('mysql:host=https://auth-db307.hostinger.com/db_routines.php?db=u392629804_groep1;dbname=u392629804_groep1', 'u392629804_usergroep1', 'RwW$o1lQ');
 
 ?>
+
+<?php
+$servername = "groep1.wtt-informatica.com";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
