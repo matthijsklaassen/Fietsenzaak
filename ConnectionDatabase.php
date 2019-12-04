1b04043efd1a948ca4645893ef1d6d243305cc6a
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-12-04 13:44:32 +0100

    Update ConnectionDatabase.php

--- ConnectionDatabase.php
+++ ConnectionDatabase.php
@@ -9,6 +9,7 @@ $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+    //function die print de waarde en beindigt het script, het is dus een echo die het ook beeindigd
 }
 echo "Connected successfully";
 ?>
