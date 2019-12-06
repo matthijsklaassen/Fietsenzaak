d1e5b9c5264db8d41a1aef90b6536e0758a248ee
a3aank <57252017+a3aank@users.noreply.github.com>
2019-12-06 10:05:58 +0100

    Update Thirdpage.php

--- Thirdpage.php
+++ Thirdpage.php
@@ -3,36 +3,38 @@ include("ConnectionDatabase.php");
 
 
 $fietsenq = "SELECT COUNT(Framenummer) FROM Fiets";
-$leverancierq = "SELECT COUNT(TL_nummer) FROM Fiets";
+$leverancierq = "SELECT COUNT(TL_nummer) FROM Toeleverancier";
 $verkoopq = "SELECT COUNT(Verkoopnummer) FROM Verkoop";
 $klantq = "SELECT COUNT(Klantmummer) FROM Klant";
 
 if ($conn->query($fietsenq) === TRUE) {
-    echo "Information succesfully retrieved";
+    echo "Information 'fiets' succesfully retrieved";
 } else {
     echo "Error: " . $fietsenq . "<br>" . $conn->error;
 }
 
 $conn->close();
 if ($conn->query($leverancierq) === TRUE) {
-    echo "Information succesfully retrieved";
+    echo "Information 'toeleverancier' succesfully retrieved";
 } else {
     echo "Error: " . $leverancierq . "<br>" . $conn->error;
 }
 $conn->close();
 
 if ($conn->query($verkoopq) === TRUE) {
-    echo "Information succesfully retrieved";
+    echo "Information 'verkoop' succesfully retrieved";
 } else {
     echo "Error: " . $verkoopq . "<br>" . $conn->error;
 }
 $conn->close();
 
 if ($conn->query($klantq) === TRUE) {
-    echo "Information succesfully retrieved";
+    echo "Information 'klant' succesfully retrieved";
 } else {
     echo "Error: " . $klantq . "<br>" . $conn->error;
 }
 $conn->close();
-
 ?>
+
+<HTML>
+    
