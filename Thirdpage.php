712cf82dd25485c4ec2d4c86a08d630bed89256a
a3aank <57252017+a3aank@users.noreply.github.com>
2019-12-06 09:57:31 +0100

    Update Thirdpage.php
    querries toevoegen

--- Thirdpage.php
+++ Thirdpage.php
@@ -1,3 +1,12 @@
 <?php 
 include("ConnectionDatabase.php");
+
+
+$fietsenq = "SELECT COUNT(Framenummer) FROM Fiets";
+$leverancierq = "SELECT COUNT(TL_nummer) FROM Fiets";
+$verkoopq = "SELECT COUNT(Verkoopnummer) FROM Verkoop";
+$klantq = "SELECT COUNT(Klantmummer) FROM Klant";
+
+
+
 ?>
