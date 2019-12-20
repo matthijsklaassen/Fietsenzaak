65e5a6006abe8c74386596c602a6245215e791ea
matthijsklaassen <57993466+matthijsklaassen@users.noreply.github.com>
2019-12-20 09:45:14 +0100

    Update Dropdownlijst.php

--- Dropdownlijst.php
+++ Dropdownlijst.php
@@ -1,1 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        
+        <link href="CSSFILE.css" rel="stylesheet" type="text/css"/>
+        <meta charset="utf-8">
+        
+        <title>Dropdownlijst</title>
+        
+    </head>
+    
+    <body>
+
+        <!-- Menubalk -->
+        <div id="menubalk">
+          <ul>
+                <li><a href="Home.php">Home</a></li>
+        	<li><a href="Formulier_toeleverancier.php">Formulier</a></li>
+          <li><a class="active" href="Dropdownlijst.php">Aantallen</a></li>
+        	<li><a href="Aantallen.php">Aantallen</a></li>
+          </ul>
+        </div>
+            
+        <h1> Dropdownlijst </h1>
+        <hr>
+        <p>
+            Op deze pagina is het mogelijk PK's aan elkaar te koppelen met dropdownlijstjes. 
+        </p>
+
+      
+                      
+    </body> 
+</html>
