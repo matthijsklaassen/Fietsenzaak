1106c3e0ed3675193d57a5fdfe4c9579900faf10
matildakolijn <57993568+matildakolijn@users.noreply.github.com>
2020-01-16 20:46:29 +0100

    Update ConnectionDatabase.php

--- ConnectionDatabase.php
+++ ConnectionDatabase.php
@@ -1,16 +1,25 @@
 <?php
 $servername = "sql307.main-hosting.eu";
+// $database = "u392629804_groep1";
+// $username = "u392629804_usergroep1";
+// $password = "RwW$o1lQ";
 $username = "%";
 $password = "GN3VEONVDx";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
+// $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     //function die print de waarde en beindigt het script, het is dus een echo die het ook beeindigd
 }
+/*
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+*/
 echo "Connected successfully";
 ?>
 
